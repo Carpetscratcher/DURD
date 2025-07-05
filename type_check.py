1e34ce79c21d98eb4ecffe0a55468f1a890bb5fe
Tafelveer <43243311+Carpetscratcher@users.noreply.github.com>
2025-07-05 15:34:25 +0200

    Update type_check.py

--- type_check.py
+++ type_check.py
@@ -1,4 +1,4 @@
-folder_path = "C:/Users/alvin_xtd9mn0/Documents/scriptie/type"
+folder_path = # (folder path)
 
 import pandas as pd
 import os
@@ -10,7 +10,6 @@ for filename in ["tcs.csv"]:
         file_path = os.path.join(folder_path, filename)
         df = pd.read_csv(file_path, quoting=csv.QUOTE_ALL)
 
-        # count the amount of types per file
         type_counts = df['type'].value_counts()
         print(f"File: {filename}")
         for t, count in type_counts.items():
